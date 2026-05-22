40afc38430c1b42f1cfc02b1da6e78df21df2587
ENAFUL <enfulemondal91@gmail.com>
2026-05-23 02:15:51 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+// src/utils/auth.js
+export const isAuthenticated = () => {
+  return localStorage.getItem('token') !== null;
+};
+
+export const logout = () => {
+  localStorage.removeItem('token');
+  window.location.href = '/login';
+};

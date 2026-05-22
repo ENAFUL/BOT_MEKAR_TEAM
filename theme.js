981c9c99a82541191ab885d2dd53e00a787e8125
ENAFUL <enfulemondal91@gmail.com>
2026-05-23 02:16:14 +0530

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,12 @@
+// src/styles/theme.js
+export const theme = {
+  colors: {
+    primary: '#00ff41', // হ্যাকার গ্রিন
+    secondary: '#ff00ff', // নিয়ন পিঙ্ক
+    bgDark: '#0a0a0a',
+    cardBg: '#151515',
+  },
+  fonts: {
+    main: 'Courier New, monospace',
+  }
+};
